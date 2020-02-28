4299299f1e80837cb8b963b6b3a42f5ac4858151
jmhaussaire <jean-matthieu.haussaire@empa.ch>
2020-02-28 10:08:14 +0100

    Add import csv

--- vid_process.py
+++ vid_process.py
@@ -6,6 +6,7 @@ import cv2
 from pascal_voc_writer import Writer as LabelWriter
 import pandas as pd
 from common import Detection
+import csv
 
 def process_vid(in_vid_name,seconds_count,seconds_skip,
                 yolo,
